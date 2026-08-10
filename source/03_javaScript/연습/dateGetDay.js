6de304f8c765b5f46be0bef0f336322d1df829b5
jack8704 <jack_guy@naver.com>
2026-08-10 18:01:16 +0900

    문서객체가져오기

---
+++ source/03_javaScript/연습/dateGetDay.js
@@ -0,0 +1,13 @@
+Date.prototype.getNumberOfDays = function(thatday){ //now가 this / openday가 thatday
+    let interval=Math.abs(this.getTime()-thatday.getTime()); // 두 시점간 밀리세컨
+    // if(this>thatday){
+    //     interval = this.getTime() - thatday.getTime();
+    // }else{
+    //     interval = thatday.getTime() - this.getTime();
+    // }
+    let day = Math.trunc(interval/(1000*60*60*24)); // 소숫점에서 짤림
+    day = Math.floor(interval/(1000*60*60*24)); // 소숫점에서 내림
+    day = Math.round(interval/(1000*60*60*24)); // 소숫점에서 반올림
+    day = Math.ceil(interval/(1000*60*60*24)); // 소숫점에서 올림
+    return day;
+};
